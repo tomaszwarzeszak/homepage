8518a382f6f12ff2f7cb1b4a711aa0022158cad2
Tomasz Warzeszak <tomekwarzeszak@gmail.com>
2020-02-05 22:50:39 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const City = "Kiczory";
+
+let name = "Tomek";
+let nazwisko = "Warzeszak";
+let age = 24;
+
+alert(`Cześć !
+Moje imię to ${name} nazwisko ${nazwisko} mam lat ${age} i pochodzę z małej wioski ${City}`);
+
+console.log (`Cześć !
+Jestem ${name}, widzę, że zaglądasz do zakładki konsoli. Świetnie !!! prawdopodobnie podobnie jak ja interesujesz się programowaniem webowym
+Miło mi Cię widzieć na mojej stronie domowej.`);
